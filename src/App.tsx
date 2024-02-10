db5b9245374619064d34e0cb58f8f5d91a6d39f7
moloudii <mhmmdalinaz@gmail.com>
2024-02-10 17:49:52 +0330

    install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route path="/sign-in" element={<SignInForm />} />
+
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
