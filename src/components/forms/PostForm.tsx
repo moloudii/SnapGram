6c8ed028a561e8e4a7b839d4074b65a2326ccfe5
moloudii <mhmmdalinaz@gmail.com>
2024-02-20 10:34:26 +0330

    FileUploader Functionality

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -19,7 +19,7 @@ const formSchema = z.object({
     message: "Username must be at least 2 characters.",
   }),
 });
-const PostForm = () => {
+const PostForm = ({ post }) => {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -66,7 +66,10 @@ const PostForm = () => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Photos</FormLabel>
               <FormControl>
-                <FileUploader />
+                <FileUploader
+                  fieldChange={field.onChange}
+                  mediaUrl={post?.imageUrl}
+                />
               </FormControl>
               <FormMessage className="shad-from_message" />
             </FormItem>
